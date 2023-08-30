2f5ef92580fcccb8f8e07a399bbb57c42ca5a51f
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-08-29 21:18:56 -0500

    added linear algebra functions to header file

---
+++ network.h
@@ -0,0 +1,62 @@
+typedef struct {
+  unsigned int rows;
+  unsigned int columns;
+  double **values;
+} Matrix;
+
+void print_matrix_row(Matrix *matrix, int row);
+
+/**
+ * Prints a matrix to standard output.
+ * 
+ * @param matrix The matrix to be printed.
+ */
+void print_matrix(Matrix *matrix);
+
+/**
+ * Prints a matrix and it's dimensions to standard output.
+ * 
+ * @param matrix The matrix to be printed.
+ */
+void print_matrix_verbose(Matrix *matrix);
+
+/**
+ * Allocates a matrix and it's values (initialized to 0) to the heap.
+ * 
+ * @param rows The number of rows in the matrix.
+ * @param columns The number of columns in the matrix.
+ * @return The allocated Matrix struct
+ */
+Matrix *allocate_empty(unsigned int rows, unsigned int columns);
+
+/**
+ * Allocates a matrix struct to the heap using the values of the 2D array 
+ * passed to the function.
+ * 
+ * @param rows The number of rows in the matrix.
+ * @param columns The number of columns in the matrix.
+ * @param arr[rows][columns] The 2D array where the matrix gets it's values
+ * @return The allocated Matrix struct
+ */
+Matrix *allocate_from_2D_arr(unsigned int rows, unsigned int columns,
+                             double arr[rows][columns]);
+
+/**
+ * Frees an allocated matrix and the values associated with it.
+ * 
+ * @param matrix The matrix to be freed
+ */
+void free_matrix(Matrix *matrix);
+
+/**
+ * Calculates the dot product of two vectors and returns the answer as a
+ * scalar.
+ * 
+ * @param vec1 A size 1 x n or n x 1 matrix to represent a vector
+ * @param vec2 A size 1 x n or n x 1 matrix to represent a vector
+ * @return The dot product of the input vectors
+ */
+double dot_product(Matrix *vec1, Matrix *vec2);
+
+void print_dot(Matrix *A, Matrix *B);
+
