dd4057f79affd49bd3a5ed7efda5086b5df52d20
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-09-02 22:07:32 -0500

    added gradient calculation test

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ all: linalg grads
 linalg: linalg.c tests/linalg_tests.c network_funcs.c 
 	clang -o linalg linalg.c tests/linalg_tests.c network_funcs.c -L/opt/homebrew/lib -lcriterion -I/opt/homebrew/include
 
-grads: linalg.c tests/linalg_tests.c network_funcs.c 
+grads: linalg.c tests/grads_tests.c network_funcs.c 
 	clang -o grads linalg.c tests/grads_tests.c network_funcs.c -L/opt/homebrew/lib -lcriterion -I/opt/homebrew/include
 
 clean:
--- tests/grads_tests.c
+++ tests/grads_tests.c
@@ -9,9 +9,39 @@ Test(zero_grad, zeros0)
   for (unsigned int i = 0; i < weights->rows; i++) {
     for (unsigned int j = 0; j < weights->columns; j++) {
       if (weights->values[i][j] != 0) {
+        free_matrix(weights);
         cr_assert(0);
       }
     }
   }
+  free_matrix(weights);
   cr_assert(1);
+}
+
+Test(input_grads, ingrad00)
+{
+  double input_arr[1][2] = {
+    {2, 1}
+  };
+  double weights_arr[2][2] = {
+    {2, 0},
+    {0, 1}
+  };
+  double output_grad[1][1] = {
+    {1}
+  };
+  Layer *in = init_layer(allocate_from_2D_arr(1, 2, input_arr), allocate_empty(1, 1), NULL, NULL);
+  Layer *l = init_layer(NULL, NULL, allocate_from_2D_arr(2, 2, weights_arr), allocate_empty(2, 1)); 
+  RELU_Layer *relu = init_RELU_layer(NULL, allocate_from_2D_arr(1, 1, output_grad));
+
+  calc_layer_output(l, in->output);
+  calc_RELU_layer(relu, l->output);
+  calc_layer_gradients_from_RELU(l, relu->output_grads);
+  calc_weight_gradients(l, in->output);
+  in->output_grads = calc_layer_input_gradients(l, in->output_grads);
+
+  cr_assert(relu->output_grads->values[0][0] == 1);
+  cr_assert(l->output_grads->values[0][0] == 1);
+  cr_assert(l->weight_grads->values[0][0] == 2);
+  cr_assert(in->output_grads->values[0][0] == 2);
 }
