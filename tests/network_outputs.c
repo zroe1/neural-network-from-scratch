1b5fa30a5621d6292221aa290142b48c5878a24f
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-09-03 13:40:54 -0500

    built out basic tests for relu layer

--- tests/network_outputs.c
+++ tests/network_outputs.c
@@ -1,7 +1,8 @@
 #include <criterion/criterion.h>
 #include "../network.h"
 
-Test(ouputs, outputs1)
+/* test for relu layer to keep positive values unchanged */
+Test(ouputs, outputs00)
 {
   double input_arr[1][2] = {
     {2, 1}
@@ -14,11 +15,41 @@ Test(ouputs, outputs1)
     {1}
   };
 
+  Matrix *in = allocate_from_2D_arr(1, 2, input_arr);
   Layer *l = init_layer(NULL, NULL, allocate_from_2D_arr(2, 2, weights_arr), NULL); 
   RELU_Layer *relu = init_RELU_layer(NULL, allocate_from_2D_arr(1, 1, output_grad));
 
-  calc_layer_output(l, allocate_from_2D_arr(1, 2, input_arr));
+  calc_layer_output(l, in);
+  calc_RELU_layer(relu, l->output);
   cr_assert(l->output->values[0][0] == 4);
+  cr_assert(relu->output->values[0][0] == 4);
+
+  free_RELU_layer(relu);
+  free_layer(l);
+}
+
+/* test for relu layer to turn negative to zero */
+Test(ouputs, outputs01)
+{
+  double input_arr[1][2] = {
+    {2, 1}
+  };
+  double weights_arr[2][2] = {
+    {-2, 0},
+    {0, 1}
+  };
+  double output_grad[1][1] = {
+    {1}
+  };
+
+  Matrix *in = allocate_from_2D_arr(1, 2, input_arr);
+  Layer *l = init_layer(NULL, NULL, allocate_from_2D_arr(2, 2, weights_arr), NULL); 
+  RELU_Layer *relu = init_RELU_layer(NULL, allocate_from_2D_arr(1, 1, output_grad));
+
+  calc_layer_output(l, in);
+  calc_RELU_layer(relu, l->output);
+  cr_assert(l->output->values[0][0] == -4);
+  cr_assert(relu->output->values[0][0] == 0);
 
   free_RELU_layer(relu);
   free_layer(l);
