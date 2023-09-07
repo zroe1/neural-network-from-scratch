32c15c0fe80f1e60cf6831e6588d76fb6c046000
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-09-06 20:27:53 -0500

    added mockup for training process (accuracy ~70%)

--- train.c
+++ train.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include "network.h"
 
-double LEARNING_RATE = 0.00001;
+double LEARNING_RATE = 0.002;
 
 void gradient_descent(Layer *layer1, Layer *layer2) {
-  // gradient_descent_on_layer(layer2, LEARNING_RATE);
+  gradient_descent_on_layer(layer2, LEARNING_RATE);
   gradient_descent_on_layer(layer1, LEARNING_RATE);
 }
 
@@ -28,39 +28,75 @@ int main() {
   Layer *l2 = init_layer(NULL, NULL, init_random_weights(129, 11), allocate_empty(129, 10));
   Squish_Layer *squish = init_squish_layer(NULL, NULL);
 
-  in->output = flattened_imgs[0];
+  // unsigned int current_img = 0;
 
-  // calc_mean_squared_loss(double output, double correct)
-  forward_pass(in, l1, relu, l2, squish);
-  backward_pass(in, l1, relu, l2, squish, 1);
-  // print_layer(l2, "layer2");
-  // print_squish_layer(squish, "squish");
+  for (unsigned int epoch = 0; epoch < 20; epoch++) {
+    double epoch_loss = 0;
 
-  double correct = 1;
-  double loss = 0;
-  for (unsigned int i = 0; i < 10; i++) {
-    if (i + 1 == correct) {
-      loss += calc_mean_squared_loss(squish->output->values[0][i], 1);
-    } else {
-      loss += calc_mean_squared_loss(squish->output->values[0][i], 0);
+    for (unsigned int current_img = 0; current_img < 60000; current_img++) {
+      in->output = flattened_imgs[current_img];
+      double correct = labels[current_img];
+      // printf("correct: %f\n", correct);
+
+      forward_pass(in, l1, relu, l2, squish);
+
+      // calculates loss for data point
+      for (unsigned int i = 0; i < 10; i++) {
+        if (i + 1 == correct) {
+          epoch_loss += calc_mean_squared_loss(squish->output->values[0][i], 1);
+        } else {
+          epoch_loss += calc_mean_squared_loss(squish->output->values[0][i], 0);
+        }
+      }
+
+      backward_pass(in, l1, relu, l2, squish, correct);
+    }
+    epoch_loss /= 60000;
+    printf("EPOCH LOSS: %f\n", epoch_loss);
+    gradient_descent(l1, l2);
+    zero_gradients(l1->weight_grads);
+    zero_gradients(l2->weight_grads);
+  }
+
+  unsigned int NUM_TEST_IMGS = 10000;
+  Matrix **test_imgs = load_MNIST_images("TRAINING_DATA/x_test.txt", NUM_TEST_IMGS);
+  Matrix **flattened_test_imgs = (Matrix **)calloc(NUM_TEST_IMGS, sizeof(Matrix *));
+  for (unsigned int i = 0; i < NUM_TEST_IMGS; i++) {
+    flattened_test_imgs[i] = flatten_matrix_and_append_one(test_imgs[i]);
+  }
+
+  double *test_labels = load_MNIST_lables("TRAINING_DATA/y_test.txt", NUM_TEST_IMGS);
+
+  // original images are no longer needed because model uses the flattened form
+  free_matrix_array(test_imgs, NUM_TEST_IMGS);
+
+  double accuracy = 0;
+  for (unsigned int current_img = 0; current_img < 10000; current_img++) {
+    in->output = flattened_test_imgs[current_img];
+    double correct = test_labels[current_img];
+    // printf("correct: %f\n", correct);
+    // print_matrix_verbose(in->output);
+
+    forward_pass(in, l1, relu, l2, squish);
+    // printf("correct: %f\n", correct);
+    double max_output = 0;
+    unsigned int max_output_idx = 0;
+    for (unsigned int i = 0; i < 10; i++) {
+      if (squish->output->values[0][i] > max_output) {
+        max_output = squish->output->values[0][i];
+        max_output_idx = i;
+      }
+    }
+    // printf("guess: %d correct%f\n", max_output_idx + 1, correct);
+    if (max_output_idx + 1 == correct) {
+      accuracy += 1;
     }
   }
-  printf("LOSS: %f\n", loss);
+  accuracy /= 10000;
+  printf("accuracy: %f\n", accuracy);
 
-  // print_matrix(l1->weight_grads);
-  // print_matrix(l2->weight_grads);
-  gradient_descent(l1, l2);
-  forward_pass(in, l1, relu, l2, squish);
-
-  loss = 0;
-  for (unsigned int i = 0; i < 10; i++) {
-    if (i + 1 == correct) {
-      loss += calc_mean_squared_loss(squish->output->values[0][i], 1);
-    } else {
-      loss += calc_mean_squared_loss(squish->output->values[0][i], 0);
-    }
-  }
-  printf("LOSS: %f\n", loss);
+  free(test_labels);
+  free_matrix_array(flattened_test_imgs, NUM_TEST_IMGS);
 
   free(labels);
   free_matrix_array(flattened_imgs, NUM_IMGS);
