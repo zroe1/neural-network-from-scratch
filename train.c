be2d94d36a8f820666d2bb7123b58e05b358ed1e
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-09-07 14:45:55 -0500

    added SGD and small code modifications

--- train.c
+++ train.c
@@ -2,20 +2,30 @@
 #include <stdlib.h>
 #include "network.h"
 
-double LEARNING_RATE = 0.002;
+double LEARNING_RATE = 0.001;
 
 void gradient_descent(Layer *layer1, Layer *layer2) {
   gradient_descent_on_layer(layer2, LEARNING_RATE);
   gradient_descent_on_layer(layer1, LEARNING_RATE);
 }
 
+void normalize_img_matrix(Matrix *img) {
+  for (unsigned int i = 0; i < img->rows; i++) {
+    for (unsigned int j = 0; j < img->columns; j++) {
+      img->values[i][j] /= 255;
+    }
+  }
+}
+
 int main() {
+  printf("LEARNING RATE: %f\n", LEARNING_RATE);
   unsigned int NUM_IMGS = 60000;
 
   Matrix **imgs = load_MNIST_images("TRAINING_DATA/x_train.txt", NUM_IMGS);
   Matrix **flattened_imgs = (Matrix **)calloc(NUM_IMGS, sizeof(Matrix *));
   for (unsigned int i = 0; i < NUM_IMGS; i++) {
     flattened_imgs[i] = flatten_matrix_and_append_one(imgs[i]);
+    normalize_img_matrix(flattened_imgs[i]);
   }
 
   // original images are no longer needed because model uses the flattened form
@@ -28,15 +38,18 @@ int main() {
   Layer *l2 = init_layer(NULL, NULL, init_random_weights(129, 11), allocate_empty(129, 10));
   Squish_Layer *squish = init_squish_layer(NULL, NULL);
 
-  // unsigned int current_img = 0;
-
-  for (unsigned int epoch = 0; epoch < 20; epoch++) {
+  for (unsigned int epoch = 0; epoch < 30; epoch++) {
+    /*
+    if (epoch != 0 && epoch % 5 == 0) {
+      LEARNING_RATE -= 0.0001;
+      printf("LEARNING RATE: %f\n", LEARNING_RATE);
+    }
+    */
     double epoch_loss = 0;
 
-    for (unsigned int current_img = 0; current_img < 60000; current_img++) {
+    for (unsigned int current_img = 0; current_img < NUM_IMGS; current_img++) {
       in->output = flattened_imgs[current_img];
       double correct = labels[current_img];
-      // printf("correct: %f\n", correct);
 
       forward_pass(in, l1, relu, l2, squish);
 
@@ -50,12 +63,12 @@ int main() {
       }
 
       backward_pass(in, l1, relu, l2, squish, correct);
+      gradient_descent(l1, l2);
+      zero_gradients(l1->weight_grads);
+      zero_gradients(l2->weight_grads);
     }
     epoch_loss /= 60000;
-    printf("EPOCH LOSS: %f\n", epoch_loss);
-    gradient_descent(l1, l2);
-    zero_gradients(l1->weight_grads);
-    zero_gradients(l2->weight_grads);
+    printf("#%d. EPOCH LOSS: %f\n", epoch + 1, epoch_loss);
   }
 
   unsigned int NUM_TEST_IMGS = 10000;
@@ -63,6 +76,7 @@ int main() {
   Matrix **flattened_test_imgs = (Matrix **)calloc(NUM_TEST_IMGS, sizeof(Matrix *));
   for (unsigned int i = 0; i < NUM_TEST_IMGS; i++) {
     flattened_test_imgs[i] = flatten_matrix_and_append_one(test_imgs[i]);
+    normalize_img_matrix(flattened_test_imgs[i]);
   }
 
   double *test_labels = load_MNIST_lables("TRAINING_DATA/y_test.txt", NUM_TEST_IMGS);
@@ -74,11 +88,8 @@ int main() {
   for (unsigned int current_img = 0; current_img < 10000; current_img++) {
     in->output = flattened_test_imgs[current_img];
     double correct = test_labels[current_img];
-    // printf("correct: %f\n", correct);
-    // print_matrix_verbose(in->output);
 
     forward_pass(in, l1, relu, l2, squish);
-    // printf("correct: %f\n", correct);
     double max_output = 0;
     unsigned int max_output_idx = 0;
     for (unsigned int i = 0; i < 10; i++) {
@@ -87,7 +98,6 @@ int main() {
         max_output_idx = i;
       }
     }
-    // printf("guess: %d correct%f\n", max_output_idx + 1, correct);
     if (max_output_idx + 1 == correct) {
       accuracy += 1;
     }
