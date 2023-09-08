639d7167951151055dd75d850987173448554e6c
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-09-07 22:16:18 -0500

    first round of adding docstrings to header function

--- network.h
+++ network.h
@@ -115,6 +115,14 @@ RELU_Layer *init_RELU_layer(Matrix *output, Matrix *output_grads);
 
 void zero_gradients(Matrix *grads);
 
+/**
+ * Initializes a Matrix with random weights between -1 and 1. The final column 
+ * is all zeros except with a one at the bottom. 
+ * 
+ * @param rows The number of rows in the matrix to be returned
+ * @param cols The number of columns in the matrix to be returned
+ * @return The random weights
+ */
 Matrix *init_random_weights(unsigned int rows, unsigned int cols);
 
 /**
@@ -127,17 +135,61 @@ Matrix *init_random_weights(unsigned int rows, unsigned int cols);
  */
 void calc_layer_output(Layer *layer, Matrix *input);
 
-Matrix *weights_matmul_gradients_subtotal(Layer *layer);
-
+/**
+ * Calculates the input gradients to a layer. If the gradients have been
+ * initialized they will be overwritten by new gradients. Otherwise, new 
+ * gradients will be allocated.
+ * 
+ * @param above_layer layer above the input gradients calculated
+ * @param rv the input gradients
+ * @return the input gradients
+ */
 Matrix *calc_layer_input_gradients(Layer *above_layer, Matrix *rv);
 
+/**
+ * Calculates the output of a RELU layer. 
+ * 
+ * @param relu an allocated RELU layer
+ * @param input the input to a RELU layer
+ */
 void calc_RELU_layer(RELU_Layer *relu, Matrix *input);
 
+/**
+ * Calculates the output gradients for a given layer, given that there is a
+ * RELU layer directly above it. 
+ * 
+ * @param relu an allocated RELU layer
+ * @param input the input to a RELU layer
+ */
 void calc_layer_gradients_from_RELU(Layer *input_layer, Matrix *RELU_grads);
 
+/**
+ * Prints a layer given it's output, output gradients, weights, and weight 
+ * gradients. The output also includes the "layer_name" passed into the
+ * function
+ * 
+ * @param layer an allocated layer
+ * @param layer_name the name of the layer to be printed
+ */
 void print_layer(Layer *layer, char *layer_name);
 
+/**
+ * Prints a RELU layer given it's output and output gradients. The output also 
+ * includes the "layer_name" passed into the function.
+ * 
+ * @param layer an allocated RELU layer
+ * @param layer_name the name of the layer to be printed
+ */
 void print_RELU_layer(RELU_Layer *layer, char *layer_name);
+
+/**
+ * Prints a "squish" layer given it's output and output gradients. The output 
+ * also includes the "layer_name" passed into the function.
+ * 
+ * @param layer an allocated "squish" layer
+ * @param layer_name the name of the layer to be printed
+ */
+void print_squish_layer(Squish_Layer *layer, char *layer_name);
 
 void calc_weight_gradients(Layer *layer, Matrix *layer_inputs);
 
@@ -166,14 +218,30 @@ void free_squish_layer(Squish_Layer *l);
 
 void calc_squish_layer(Squish_Layer *layer, Matrix *inputs);
 
-void print_squish_layer(Squish_Layer *layer, char *layer_name);
-
 void calc_layer_gradients_from_squish(Layer *input_layer, Squish_Layer *squish);
 
+/**
+ * Loads the MNIST lables from a formated text file.
+ * 
+ * @param filename Name of the file to be read
+ * @param num_lines The number of expected lines
+ */
 double *load_MNIST_lables(char *filename, unsigned int num_lines);
 
+/**
+ * Loads the MNIST images from a formated text file.
+ * 
+ * @param filename Name of the file to be read
+ * @param num_imgs The number of expected images
+ */
 Matrix **load_MNIST_images(char *filename, unsigned int num_imgs);
 
+/**
+ * Frees all the data assocated with an array of Matrix structs.
+ * 
+ * @param matrix_arr Matrix array to be freed
+ * @param len Number of matrices in the array
+ */
 void free_matrix_array(Matrix **matrix_arr, unsigned int len);
 
 Matrix *flatten_matrix_and_append_one(Matrix *matrix);
