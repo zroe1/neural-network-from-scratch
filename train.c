dbad7d973d1b1fd9f4f9e0dd1c5c5aef7323a876
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-09-09 14:36:36 -0500

    made train.c automatically load weights

--- train.c
+++ train.c
@@ -4,6 +4,16 @@
 
 double LEARNING_RATE = 0.001;
 
+void save_weights(Matrix *weights, char *filename) {
+  FILE *weights_file = fopen(filename, "w");
+
+  for (unsigned int i = 0; i < weights->rows; i++) {
+    for (unsigned int j = 0; j < weights->columns - 1; j++) {
+      fprintf(weights_file, "%f\n", weights->values[i][j]);
+    }
+  }
+}
+
 int main() {
   printf("LEARNING RATE: %f\n", LEARNING_RATE);
   unsigned int NUM_IMGS = 60000;
@@ -25,7 +35,7 @@ int main() {
   Layer *l2 = init_layer(NULL, NULL, init_random_weights(129, 11), allocate_empty(129, 10));
   Squish_Layer *squish = init_squish_layer(NULL, NULL);
 
-  for (unsigned int epoch = 0; epoch < 30; epoch++) {
+  for (unsigned int epoch = 0; epoch < 20; epoch++) {
     double epoch_loss = 0;
 
     for (unsigned int current_img = 0; current_img < NUM_IMGS; current_img++) {
@@ -42,25 +52,26 @@ int main() {
           epoch_loss += calc_mean_squared_loss(squish->output->values[0][i], 0);
         }
       }
-
       backward_pass(in, l1, relu, l2, squish, correct);
-      gradient_descent(l1, l2, LEARNING_RATE);
-      zero_gradients(l1->weight_grads);
-      zero_gradients(l2->weight_grads);
+      if (current_img % 128 == 0) {
+        gradient_descent(l1, l2, LEARNING_RATE);
+        zero_gradients(l1->weight_grads);
+        zero_gradients(l2->weight_grads);
+      }
     }
     epoch_loss /= 60000;
     printf("#%d. EPOCH LOSS: %f\n", epoch + 1, epoch_loss);
   }
 
   unsigned int NUM_TEST_IMGS = 10000;
-  Matrix **test_imgs = load_MNIST_images("TRAINING_DATA/x_test.txt", NUM_TEST_IMGS);
+  Matrix **test_imgs = load_MNIST_images("TEST_DATA/x_test.txt", NUM_TEST_IMGS);
   Matrix **flattened_test_imgs = (Matrix **)calloc(NUM_TEST_IMGS, sizeof(Matrix *));
   for (unsigned int i = 0; i < NUM_TEST_IMGS; i++) {
     flattened_test_imgs[i] = flatten_matrix_and_append_one(test_imgs[i]);
     normalize_img_matrix(flattened_test_imgs[i]);
   }
 
-  double *test_labels = load_MNIST_lables("TRAINING_DATA/y_test.txt", NUM_TEST_IMGS);
+  double *test_labels = load_MNIST_lables("TEST_DATA/y_test.txt", NUM_TEST_IMGS);
 
   // original images are no longer needed because model uses the flattened form
   free_matrix_array(test_imgs, NUM_TEST_IMGS);
@@ -85,6 +96,9 @@ int main() {
   }
   accuracy /= 10000;
   printf("accuracy: %f\n", accuracy);
+
+  save_weights(l1->weights, "layer1_weights.txt");
+  save_weights(l2->weights, "layer2_weights.txt");
 
   free(test_labels);
   free_matrix_array(flattened_test_imgs, NUM_TEST_IMGS);
